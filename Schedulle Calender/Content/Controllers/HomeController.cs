c51981ec04bdc83169fde276b7497a4b5602da45
Sagal Nunow <Sagal.Nunow@icstars-columbus.org>
2018-09-24 13:45:52 -0400

    first commit

---
+++ Schedulle Calender/Content/Controllers/HomeController.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+
+namespace Schedulle_Calender.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public JsonResult GetEvents()
+        {
+            using (myDatabaseEntities1 dc = new myDatabaseEntities1())
+            {
+                var events = dc.Events.ToList();
+                return new JsonResult { Data = events, JsonRequestBehavior = JsonRequestBehavior.AllowGet };
+            }
+        }
+        [HttpPost]
+        public JsonResult SaveEvent(Event e)
+        {
+            var status = false;
+            using (myDatabaseEntities1 dc = new myDatabaseEntities1())
+            {
+               
+                if (e.EventId > 0)
+                {
+                    //Update the event
+                    var v = dc.Events.Where(a => a.EventId == e.EventId).FirstOrDefault();
+                    if (v != null)
+                    {
+                        v.Subject = e.Subject;
+                        v.Start = e.Start;
+                        v.End = e.End;
+                        v.Description = e.Description;
+                        v.IsFullDay = e.IsFullDay;
+                        v.ThemeColor = e.ThemeColor;
+                    }
+                }
+                else
+                {
+                    dc.Events.Add(e);
+                }
+                dc.SaveChanges();
+                status = true;
+            }
+            return new JsonResult { Data = new { status = status } };
+        }
+        [HttpPost]
+        public JsonResult DeleteEvent(int eventId)
+        {
+            var status = false;
+            using (myDatabaseEntities1 dc = new myDatabaseEntities1())
+            {
+                var v = dc.Events.Where(a => a.EventId == eventId).FirstOrDefault();
+                if (v != null)
+                {
+                    dc.Events.Remove(v);
+                    dc.SaveChanges();
+                    status = true;
+                }
+            }
+            return new JsonResult { Data = new { status = status } };
+        }
+    }
+}
+    
---
+++ Schedulle Calender/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Schedulle_Calender.MvcApplication" Language="C#" %>
